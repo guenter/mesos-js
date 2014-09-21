aab94b932795661197934844b2e46e54c88a54d5
Cassy Jens <jens.cass@gmail.com>
2014-09-21 12:01:51 -0700

    initial Grunt tasks

---
+++ grunt/copy.js
@@ -0,0 +1,19 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.config.set('copy', {
+        src: {
+            files: [{
+                expand: true,
+                cwd: '<%= paths.src %>/',
+                src: [
+                    '**/*',
+                    '**/*.min.js'
+                ],
+                dest: '<%= paths.dist %>/'
+            }]
+        }
+    });
+
+    return grunt;
+};
