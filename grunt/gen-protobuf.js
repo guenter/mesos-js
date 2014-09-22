40ee8f826cc2480968cce442cb7d427ffb58fa03
Cassy Jens <jens.cass@gmail.com>
2014-09-21 19:34:15 -0700

    make generated dir if DNE

--- grunt/gen-protobuf.js
+++ grunt/gen-protobuf.js
@@ -7,11 +7,20 @@
      * Calls proto2js on all .proto files in src proto dir
      */
     grunt.registerTask('gen-protobuf', function () {
+      var fs = require('fs');
+      var mkdirp = require('mkdirp');
+      var path = require('path');
       var readdir = require('readdir');
       var paths = grunt.config.data.paths;
       var files = readdir.readSync(paths.srcProto, ['**.proto']);
 
       files.forEach(function (file) {
+        var p = paths.destProto + '/' + path.dirname(file);
+
+        if (!fs.existsSync(p)) {
+            mkdirp.sync(p);
+        }
+
         grunt.task.run('exec:gen_protobuf:' + file);
       });
     });
